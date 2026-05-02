f46ed56bab399cb04bc4bb00a78f6ab8795bd322
Jakob Mifflin <jmiffli@siue.edu>
2026-05-02 18:39:05 -0500

    Scoreboard

--- Assets/_Scripts/Scoreboard.cs
+++ Assets/_Scripts/Scoreboard.cs
@@ -1,15 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText;
+    [Header("Per Hole Data")]
+    public TextMeshProUGUI[] parTexts;
+    public TextMeshProUGUI[] strokeTexts;
+
+    [Header("Overall Score")]
+    public TextMeshProUGUI overallScoreText;
 
     void Start()
     {
-        if (GameManager.Instance != null)
+        if (GameManager.Instance == null) return;
+
+        List<HoleResult> results = GameManager.Instance.GetHoleResults();
+
+        int totalScore = 0;
+
+        for (int i = 0; i < results.Count; i++)
         {
-            scoreText.text = GameManager.Instance.GetFinalScorecard();
+            if (i < parTexts.Length)
+                parTexts[i].text = results[i].par.ToString();
+
+            if (i < strokeTexts.Length)
+                strokeTexts[i].text = results[i].strokes.ToString();
+
+            totalScore += results[i].scoreRelativeToPar;
+        }
+
+        if (overallScoreText != null)
+        {
+            overallScoreText.text = totalScore > 0
+                ? "+" + totalScore
+                : totalScore.ToString();
         }
     }
 }
